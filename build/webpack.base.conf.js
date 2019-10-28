756f4ce83284170ba990496dede52e0ecefe11a1
Alex-Smil <alexsmile39@gmail.com>
2019-10-28 09:24:01 +0300

    Урок 6 - Полная настройка шаблонизатора pug. Использование в верстке и не только

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -13,8 +13,8 @@ const PATHS = {
     assets: 'assets/',
 };
 
-const PAGES_DIR = `${PATHS.src}/html`;
-const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.html'));
+const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
 
 module.exports = {
     // Необходимо для того чтобы получить доступ к PATHS из других конфигов
@@ -55,6 +55,9 @@ module.exports = {
                         scss: 'vue-style-loader!css-loader!sass-loader'
                     }
                 }
+            },{
+                test: /\.pug$/,
+                loader: 'pug-loader'
             }, {
                 test: /\.scss$/,
                 use: [
@@ -117,7 +120,7 @@ module.exports = {
         // best way to create pages: https://github.com/vedees/webpack-template/blob/master/README.md#third-method-best
         ...PAGES.map(page => new HtmlWebpackPlugin({
             template: `${PAGES_DIR}/${page}`,
-            filename: `./${page}`
+            filename: `./${page.replace(/\.pug/, '.html')}`
         }))
     ]
 }
